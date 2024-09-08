d6b725223a6a29216901d4f5a31f96c1923d0150
Vijaianand <vijaianand@cgvakindia.com>
2024-09-08 09:56:02 +0530

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MyFirstMAUIApp;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MyFirstMAUIApp;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void BtnAbout_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new AboutPage());
+        SemanticScreenReader.Announce(BtnAbout.Text);
+    }
+}
+
